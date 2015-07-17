f16c1c25ee0d035a50aa6dcb40cbf7c3e8f79ec1
haya14busa <hayabusa1419@gmail.com>
2015-07-18 01:32:15 +0900

    Implement method/path/protocol in Log class

--- scala/src/main/scala/hatena/intern/Exercise1.scala
+++ scala/src/main/scala/hatena/intern/Exercise1.scala
@@ -1,9 +1,14 @@
 package hatena.intern
 
 case class Log(host: String, user: String, epoch: Int, req: String, status: Int, size: Int, referer: String) {
-  def method: String = ???
-  def path: String = ???
-  def protocol: String = ???
+  // xxx Value. Just `val` definitions without `V` suffix suttisfy the
+  // requirements but defining them with `def` is more readable I guess
+  private val Array(methodV, pathV, protocolV) = this.req.split(" ")
+
+  def method: String = methodV
+  def path: String = pathV
+  def protocol: String = protocolV
+
   def uri: String = ???
   def time: String = ???
 }
