032d83514b8ab27d7692bab51321e7bd413f53fc
haya14busa <hayabusa1419@gmail.com>
2015-07-18 01:55:33 +0900

    Implement time method in Log class

--- scala/src/main/scala/hatena/intern/Exercise1.scala
+++ scala/src/main/scala/hatena/intern/Exercise1.scala
@@ -1,14 +1,21 @@
 package hatena.intern
 
+import java.util.TimeZone
+import java.text.SimpleDateFormat
+
 case class Log(host: String, user: String, epoch: Int, req: String, status: Int, size: Int, referer: String) {
   // xxx Value. Just `val` definitions without `V` suffix suttisfy the
   // requirements but defining them with `def` is more readable I guess
   private val Array(methodV, pathV, protocolV) = this.req.split(" ")
+
+  private val gmtFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+  gmtFormatter.setTimeZone(TimeZone.getTimeZone("GMT"))
 
   def method: String = methodV
   def path: String = pathV
   def protocol: String = protocolV
 
   def uri: String = s"http://$host$path"
-  def time: String = ???
+
+  def time: String = gmtFormatter.format(this.epoch.toLong * 1000)
 }
