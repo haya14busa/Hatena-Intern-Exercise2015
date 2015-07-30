d330b73382016843f6461f32f3b9e40f5ee1e8c9
haya14busa <hayabusa1419@gmail.com>
2015-07-30 19:31:04 +0900

    Implement groupByUser at LogCounter

--- scala/src/main/scala/hatena/intern/Exercise3.scala
+++ scala/src/main/scala/hatena/intern/Exercise3.scala
@@ -5,5 +5,5 @@ case class LogCounter(logs: Iterable[Log]) {
   def countError: Int = (logs filter (log => 500 <= log.status && log.status < 600)).size
 
   type User = String
-  def groupByUser: Map[User, Iterable[Log]] = ???
+  def groupByUser: Map[User, Iterable[Log]] = logs groupBy (_.user getOrElse "guest")
 }
