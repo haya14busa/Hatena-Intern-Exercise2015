b5136982c4b6b445d653c6d1577559c7d8e7364d
haya14busa <hayabusa1419@gmail.com>
2015-07-30 19:04:11 +0900

    spec: DRY test for Exercise2

--- scala/src/test/scala/hatena/intern/Exercise2.scala
+++ scala/src/test/scala/hatena/intern/Exercise2.scala
@@ -9,6 +9,17 @@ import scalaz._
 
 class Exercise2Spec extends UnitSpec with ValidationMatchers with DiagrammedAssertions {
 
+  // helper
+  import LtsvParser.ValidOrLtsvParserException
+  private def containsErrorMsg[X](validation: ValidOrLtsvParserException[X], msg: String): Unit =
+    containsErrorMsg(validation, List(msg))
+
+  private def containsErrorMsg[X](validation: ValidOrLtsvParserException[X], msgs: List[String]): Unit =
+    validation match {
+      case Failure(es) => msgs foreach (msg => assert(es.map(_.getMessage()).list contains msg))
+      case _ => fail("it should be failure")
+    }
+
   describe("LTSV Parser") {
 
     describe(".parseLine") {
@@ -32,46 +43,22 @@ class Exercise2Spec extends UnitSpec with ValidationMatchers with DiagrammedAsse
 
       it("should return appropriate error with invalid LTSV") {
         val line = "user:-\treferer:-\tepoch:1372694390\thost:127.0.0.1\treq:GET /apache_pb.gif HTTP/1.0\tstatus:200"
-        import scalaz._
-        LtsvParser.parseLine(line) match {
-          case Failure(es) =>
-            assert(es.head.getMessage() == "`size` field not found")
-          case _ =>
-            fail("it should be faulure")
-        }
+        containsErrorMsg(LtsvParser.parseLine(line), "`size` field not found")
       }
 
       it("should return appropriate error with invalid LTSV with line number") {
         val line = "user:-\treferer:-\tepoch:1372694390\thost:127.0.0.1\treq:GET /apache_pb.gif HTTP/1.0\tstatus:200"
-        import scalaz._
-        LtsvParser.parseLine(line, Some(14)) match {
-          case Failure(es) =>
-            assert(es.head.getMessage() == "at line 14: `size` field not found")
-          case _ =>
-            fail("it should be faulure")
-        }
+        containsErrorMsg(LtsvParser.parseLine(line, Some(14)), "at line 14: `size` field not found")
       }
 
       it("should return appropriate error with invalid req parameter") {
         val line = "user:-\treferer:-\tepoch:1372694390\thost:127.0.0.1\treq:/notfound.gif HTTP/1.0\tstatus:200\tsize:2326"
-        LtsvParser.parseLine(line) match {
-          case Failure(es) =>
-            assert(es.map(_.getMessage()).list contains "the `req` parameter is invalid: /notfound.gif HTTP/1.0")
-          case _ =>
-            fail("it should be faulure")
-        }
+        containsErrorMsg(LtsvParser.parseLine(line), "the `req` parameter is invalid: /notfound.gif HTTP/1.0")
       }
 
       it("should return multiple appropriate errors with invalid LTSV") {
         val line = "user:-\tepoch:1372694390\thost:127.0.0.1\treq:GET /apache_pb.gif HTTP/1.0\tstatus:200"
-        import scalaz._
-        LtsvParser.parseLine(line) match {
-          case Failure(es) =>
-            assert(es.map(_.getMessage()).list contains "`size` field not found")
-            assert(es.map(_.getMessage()).list contains "`referer` field not found")
-          case _ =>
-            fail("it should be faulure")
-        }
+        containsErrorMsg(LtsvParser.parseLine(line), List("`size` field not found", "`referer` field not found"))
       }
 
     }
@@ -111,26 +98,18 @@ class Exercise2Spec extends UnitSpec with ValidationMatchers with DiagrammedAsse
     describe(".parseOrError which returns error") {
 
       it("LTSVファイルが存在しない場合") {
-        LtsvParser.parseOrError("/path/doesn't/exist") match {
-          case Failure(es) =>
-            assert(es.map(_.getMessage()).list contains "/path/doesn't/exist not found")
-          case _ =>
-            fail("it should be faulure")
-        }
+        containsErrorMsg(LtsvParser.parseOrError("/path/doesn't/exist"), "/path/doesn't/exist not found")
       }
 
       describe("LTSVファイルが正しくパースできない形式の場合") {
         it("It shows error line number") {
           val filePath = (scalax.file.Path(".").toAbsolute.parent.flatMap(_.parent).get / "sample_data" / "log.error.ltsv").toURI.getRawPath
-          LtsvParser.parseOrError(filePath) match {
-            case Failure(es) =>
-              assert(es.map(_.getMessage()).list contains "at line 0: `user` field not found")
-              assert(es.map(_.getMessage()).list contains "at line 0: `epoch` is not number")
-              assert(es.map(_.getMessage()).list contains "at line 2: `size` field not found")
-              assert(es.map(_.getMessage()).list contains "at line 4: the `req` parameter is invalid: /notfound.gif HTTP/1.0")
-            case _ =>
-              fail("it should be faulure")
-          }
+          containsErrorMsg(LtsvParser.parseOrError(filePath), List(
+            "at line 0: `user` field not found",
+            "at line 0: `epoch` is not number",
+            "at line 2: `size` field not found",
+            "at line 4: the `req` parameter is invalid: /notfound.gif HTTP/1.0"
+          ))
         }
       }
 
