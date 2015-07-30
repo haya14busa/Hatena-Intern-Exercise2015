cd33fd9284930b86358046df4c80e481a5cb0806
haya14busa <hayabusa1419@gmail.com>
2015-07-30 19:16:36 +0900

    Implement countError

--- scala/src/main/scala/hatena/intern/Exercise3.scala
+++ scala/src/main/scala/hatena/intern/Exercise3.scala
@@ -1,7 +1,8 @@
 package hatena.intern
 
 case class LogCounter(logs: Iterable[Log]) {
-  def countError: Int = ???
+  // count HTTP server error
+  def countError: Int = (logs filter (log => 500 <= log.status && log.status < 600)).size
 
   type User = String
   def groupByUser: Map[User, Iterable[Log]] = ???
